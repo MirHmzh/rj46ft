6bae5d3b4a88ce2d3bac3c9835185eb99f26e030
System Administrator <corn.row81@gmail.com>
2021-04-10 19:37:48 +0700

    update path

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rj46ft';
+$config['base_url'] = 'http://192.168.117.128/rj46ft';
 
 /*
 |--------------------------------------------------------------------------
